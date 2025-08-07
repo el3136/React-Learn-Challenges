9f772a9679f094644e93dfa9a14c65206484761f
Eric Lin <el3136@nyu.edu>
2025-08-07 15:26:38 -0400

    Action Types: use variables for autocomplete

--- todo-list-assignment/src/store/store.js
+++ todo-list-assignment/src/store/store.js
@@ -8,25 +8,23 @@ const initialState = {
   inputValue: ""
 };
 
-// Action Types
-const actionTypes = [
-  "SET_INPUT",
-  "ADD_TODO",
-  "DELETE_TODO",
-  "TOGGLE_TODO",
-  "EDIT_TODO",
-  "CHANGE_EDIT_TITLE",
-  "SAVE_TODO",
-  "CANCEL_EDIT",
-];
+// Action Types: use variables for autocomplete
+const SET_INPUT = "SET_INPUT";
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const EDIT_TODO = "EDIT_TODO";
+const CHANGE_EDIT_TITLE = "CHANGE_EDIT_TITLE";
+const SAVE_TODO = "SAVE_TODO";
+const CANCEL_EDIT = "CANCEL_EDIT";
 
 // Reducer
 let nextId = 1;
 function todoReducer(state = initialState, action) {
   switch (action.type) {
-    case "SET_INPUT":
+    case SET_INPUT:
       return { ...state, inputValue: action.payload };
-    case "ADD_TODO":
+    case ADD_TODO:
       if (!state.inputValue.trim()) return state;
       return {
         ...state,
@@ -42,30 +40,30 @@ function todoReducer(state = initialState, action) {
         ],
         inputValue: ""
       };
-    case "DELETE_TODO":
+    case DELETE_TODO:
       return { ...state, todos: state.todos.filter(todo => todo.id !== action.payload) };
-    case "TOGGLE_TODO":
+    case TOGGLE_TODO:
       return {
         ...state,
         todos: state.todos.map(todo =>
           todo.id === action.payload ? { ...todo, done: !todo.done } : todo
         )
       };
-    case "EDIT_TODO":
+    case EDIT_TODO:
       return {
         ...state,
         todos: state.todos.map(todo =>
           todo.id === action.payload ? { ...todo, edit: true, editingTitle: todo.title } : todo
         )
       };
-    case "CHANGE_EDIT_TITLE":
+    case CHANGE_EDIT_TITLE:
       return {
         ...state,
         todos: state.todos.map(todo =>
           todo.id === action.payload.id ? { ...todo, editingTitle: action.payload.value } : todo
         )
       };
-    case "SAVE_TODO":
+    case SAVE_TODO:
       return {
         ...state,
         todos: state.todos.map(todo =>
@@ -74,7 +72,7 @@ function todoReducer(state = initialState, action) {
             : todo
         )
       };
-    case "CANCEL_EDIT":
+    case CANCEL_EDIT:
       return {
         ...state,
         todos: state.todos.map(todo =>
@@ -88,7 +86,7 @@ function todoReducer(state = initialState, action) {
 
 // Middleware: Format ADD_TODO title
 const titleFormatterMiddleware = store => next => action => {
-  if (action.type === "ADD_TODO") {
+  if (action.type === ADD_TODO) {
     const date = new Date();
     const timestamp = `Added at ${date}:`;
     action.payload = `${timestamp} ${store.getState().inputValue}`;
